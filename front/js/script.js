d1e8aede1c3658ffa58d137c7a5d3c0715eb4558
Haben Mebrahtom <dmhaben@gmail.com>
2022-05-20 16:38:31 +0200

    fetching products from the api in js and implement it in the html file

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,32 @@
 const apiUrl = "http://localhost:3000/api/products";
 
+const items = document.getElementById('items')
+
 const getData = async () => {
-    
+    try {
+
+        const response = await fetch(apiUrl);
+         const jsonResponse = await response.json();
+        
+        jsonResponse.forEach(item => {
+            const item = document.createElement('div');
+
+            item.innerHTML += `
+             <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altText}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`;
+        })
+
+
+        items.appendChild(item);
+
+    } catch(error) {
+        document.html.innerHTML = error;
+    }
 }
 
 getData();
