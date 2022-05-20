5ef18bd48910473467d5a3901260ee184b4d2a07
Haben Mebrahtom <dmhaben@gmail.com>
2022-05-20 20:42:22 +0200

    fetching an item image, name, price

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,28 @@
 const apiUrl = "http://localhost:3000/api/products";
 
-const params = new URLSearchParams(window.location.serach);
-const productId = params.get(id);
+const params = new URLSearchParams(document.location.search);
+const productId = params.get('id');
+
+const productImage = document.getElementsByClassName('item__img');
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const description = document.getElementById('description');
+
+
+const getProduct = async () => {
+    try {
+        const response = await fetch(apiUrl + '/' + productId)
+        const jsonResponse = await response.json();
+
+         productImage.innerHTML += `
+        <img src="${jsonResponse.imageUrl}" alt="${jsonResponse.altText}">`;
+
+        description.innerHTML += `${jsonResponse.description}`;
+        title.innerHTML += `${jsonResponse.name}`;
+        price.innerHTML += `${jsonResponse.price}`;
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+getProduct();
